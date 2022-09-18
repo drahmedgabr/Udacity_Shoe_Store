15e1cf929a969b001511f717a97a83220e0f6918
drahmedgabr <dr.ahmedgabr@yahoo.com>
2022-09-19 01:23:31 +0200

    progress

--- starter/app/src/main/java/com/udacity/shoestore/viewmodel/ShoeModel.kt
+++ starter/app/src/main/java/com/udacity/shoestore/viewmodel/ShoeModel.kt
@@ -7,18 +7,29 @@ import com.udacity.shoestore.models.Shoe
 
 class ShoeModel : ViewModel() {
 
-    private var _shoeList = mutableListOf<Shoe>(Shoe("Shoe1", 36.5, "Adidas", "Brown Color"), Shoe("Shoe2", 42.5, "Nike", "White Color"), Shoe("Shoe3", 32.5, "Puma", "Black Color"))
+    private var _shoeList = mutableListOf<Shoe>(Shoe("Shoe1", "36.5", "Adidas", "Brown Color"), Shoe("Shoe2", "42.5", "Nike", "White Color"), Shoe("Shoe3", "32.5", "Puma", "Black Color"))
 
     private var _innerShoeData = MutableLiveData<List<Shoe>>()
 
     val shoeData: LiveData<List<Shoe>>
         get() = _innerShoeData
 
+    private var _shoeAdded = MutableLiveData<Boolean>()
+
+    val isShoeAdded: LiveData<Boolean>
+    get() = _shoeAdded
+
     init {
         _innerShoeData.value = _shoeList
+        _shoeAdded.value = false
     }
 
-    fun addShoe(name: String, size: Double, company: String, description: String) {
-        _shoeList.add(Shoe(name, size, company, description))
+    fun addShoe(shoe: Shoe) {
+        _shoeList.add(Shoe(shoe.name, shoe.size, shoe.company, shoe.description))
+        _shoeAdded.value = true
+    }
+
+    fun reset(){
+        _shoeAdded.value = false
     }
 }
--- starter/app/src/main/java/com/udacity/shoestore/views/DetailFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/views/DetailFragment.kt
@@ -1,39 +1,57 @@
 package com.udacity.shoestore.views
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
 import androidx.navigation.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentDetailBinding
 import com.udacity.shoestore.databinding.FragmentLoginBinding
+import com.udacity.shoestore.models.Shoe
 import com.udacity.shoestore.viewmodel.ShoeModel
 
 
 class DetailFragment : Fragment() {
 
     lateinit var shoeModel: ShoeModel
+    lateinit var binding: FragmentDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        val binding = DataBindingUtil.inflate<FragmentDetailBinding>(
+        binding = DataBindingUtil.inflate<FragmentDetailBinding>(
             inflater, R.layout.fragment_detail, container, false
         )
 
-        shoeModel = ViewModelProvider(requireActivity()).get(ShoeModel::class.java)
+        binding.shoe = Shoe()
 
-        binding.shoeModel = shoeModel
+        binding.addButton.setOnClickListener {
+            shoeModel.addShoe(binding.shoe!!)
+        }
 
         binding.cancelButton.setOnClickListener {
             navigateUp()
         }
+
+        shoeModel = ViewModelProvider(requireActivity()).get(ShoeModel::class.java)
+
+        shoeModel.isShoeAdded.observe(this, Observer { hasAdded ->
+            if(hasAdded) {
+                navigateUp()
+                Toast.makeText(context, "Shoe Added", Toast.LENGTH_LONG).show()
+                shoeModel.reset()
+            }
+        })
 
         return binding.root
 
@@ -42,5 +60,4 @@ class DetailFragment : Fragment() {
     fun navigateUp(){
         view!!.findNavController().navigateUp()
     }
-
 }
